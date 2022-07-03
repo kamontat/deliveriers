e5c77352f37899edf34e43f42af811bfd0c90b9a
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-07-03 21:03:32 +0700

    fix review not found when initate phase

--- src/routes/reviews/index.svelte
+++ src/routes/reviews/index.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import type { Reviews } from "$lib/reviews/models";
-
-  import { user, ensureUser } from "$lib/authentication";
+  import { user } from "$lib/authentication";
   import { request, REVIEWS_LIST_FNAME } from "$lib/repositories";
 
   let name: string = "Reviews";
 
-  const reviews = request(ensureUser($user), REVIEWS_LIST_FNAME);
+  const reviews = request($user, REVIEWS_LIST_FNAME);
 </script>
 
 <svelte:head>
@@ -15,11 +13,13 @@
 
 <h1>Searching reviews</h1>
 
-{#await reviews}
-  <p>loading...</p>
-{:then reviews}
-  {reviews.total}
-  {#each reviews.rows as row}
-    <p>{row.id} - {row.star} - {row.description}</p>
-  {/each}
-{/await}
+{#if reviews}
+  {#await reviews}
+    <p>loading...</p>
+  {:then reviews}
+    {reviews.total}
+    {#each reviews.rows as row}
+      <p>{row.id} - {row.star} - {row.description}</p>
+    {/each}
+  {/await}
+{/if}
