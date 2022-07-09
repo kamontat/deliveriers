731ee0656fd9c0cbae4d69a08fad9e88feac04b8
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-07-09 23:56:44 +0700

    fix: signin error on desktop not show correctly

--- src/libs/layouts/header-desktop.svelte
+++ src/libs/layouts/header-desktop.svelte
@@ -13,10 +13,10 @@
   name="desktop"
   class="flex flex-row justify-between items-center flex-none bg-gray-800 text-gray-200 h-16 px-2 py-3"
 >
-  <div class="flex justify-center content-center px-5">
+  <div class="px-5">
     <a href="/" class="m-auto">Deliveriers</a>
   </div>
-  <div class="flex justify-center content-center px-5">
+  <div class="flex items-center px-5">
     {#if $isSignin}
       <span class="m-auto">@{getUsername($user)}</span>
       <button
@@ -26,14 +26,19 @@
         disabled={loading}>Signout</button
       >
     {:else}
+      {#if errorMessage}
+        <span class="text-sm">{errorMessage}</span>
+      {/if}
       <input
-        class="mx-1 py-1 px-2 rounded-md text-gray-900 outline-none border border-gray-300 focus:border-sky-600"
+        class="mx-1 py-1 px-2 rounded-md text-gray-900 outline-none border-2 border-gray-300 focus:border-sky-600"
+        class:border-red-500={errorMessage !== ""}
         type="text"
         placeholder="Enter your email"
         bind:value={email}
       />
       <input
         class="mx-1 py-1 px-2 rounded-md text-gray-900 outline-none border-2 border-gray-300 focus:border-sky-600"
+        class:border-red-500={errorMessage !== ""}
         type="password"
         placeholder="Enter your password"
         bind:value={password}
@@ -44,9 +49,6 @@
         on:click={onSignin}
         disabled={loading}>Signin</button
       >
-      {#if errorMessage}
-        <span class="text-sm">{errorMessage}</span>
-      {/if}
     {/if}
   </div>
 </header>
